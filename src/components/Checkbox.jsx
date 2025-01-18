02b0a41a61a761a5f5076faddfe1dd5402502beb
Phuong-Thanh Ho <hophuongthanh028@gmail.com>
2025-01-17 23:07:54 -0500

    feat: configuration toggles initial checkbox

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './styles/Checkbox.css'
+const Checkbox = (props) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
+  return (
+    <div className="checkbox-container">
+        {props.children}
+      <input
+          type="checkbox"
+          checked={checked}
+          onChange={handleChange}
+        />
+    </div>
+  )
+
+};
+
+export default Checkbox;
